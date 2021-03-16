9b4653f9ddc32a7e11565d521d2f1a10a4d2861d
Vinicius Lopes <viniciuslopes_2000@hotmail.com>
2021-03-15 22:50:14 -0300

    Modify toString

--- src/br/unicap/compilador/lexico/Token.java
+++ src/br/unicap/compilador/lexico/Token.java
@@ -7,24 +7,24 @@ public class Token {
     public static final String TK_LETRA = "Letra";
     public static final String TK_FLOAT = "Float";
     public static final String TK_CHAR = "Character";
-    public static final String TK_DIGIT = "Dígito";
+    public static final String TK_DIGIT = "Digito";
     public static final String TK_OPERATOR_relacional_maior = "Operador maior";
     public static final String TK_OPERATOR_relacional_menor = "Operador menor";
     public static final String TK_OPERATOR_relacional_maior_igual = "Operador maior igual";
     public static final String TK_OPERATOR_relacional_menor_igual = "Operador menor igual";
-    public static final String TK_OPERATOR_relacional_diferenca = "Operador diferença";
+    public static final String TK_OPERATOR_relacional_diferenca = "Operador diferenca";
     public static final String TK_OPERATOR_aritmetrico_mais= "Operador mais";
     public static final String TK_OPERATOR_aritmetrico_menos= "Operador menos";
-    public static final String TK_OPERATOR_aritmetrico_multiplicacao= "Operador multiplicação";
-    public static final String TK_OPERATOR_aritmetrico_divisao= "Operador divisão";
+    public static final String TK_OPERATOR_aritmetrico_multiplicacao= "Operador multiplicacao";
+    public static final String TK_OPERATOR_aritmetrico_divisao= "Operador divisao";
     public static final String TK_OPERATOR_igual= "Operador igual";
-    public static final String TK_OPERATOR_atribuidor= "Operador atribuição";
-    public static final String TK_CARACTER_especial_abre_parenteses = "Abre parênteses";
-    public static final String TK_CARACTER_especial_fecha_parenteses = "Fecha parênteses";
+    public static final String TK_OPERATOR_atribuidor= "Operador atribuicao";
+    public static final String TK_CARACTER_especial_abre_parenteses = "Abre parenteses";
+    public static final String TK_CARACTER_especial_fecha_parenteses = "Fecha parenteses";
     public static final String TK_CARACTER_especial_abre_chave = "Abre chave";
     public static final String TK_CARACTER_especial_fecha_chave = "Fecha chave";
-    public static final String TK_CARACTER_especial_virgula = "Vírgula";
-    public static final String TK_CARACTER_especial_pontovirgula = "Ponto e vírgula";
+    public static final String TK_CARACTER_especial_virgula = "Virgula";
+    public static final String TK_CARACTER_especial_pontovirgula = "Ponto e virgula";
     public static final String TK_PALAVRA_reservada = "Palavra reservada";
 
     private String type;
@@ -58,6 +58,6 @@ public class Token {
 
     @Override
     public String toString() {
-        return "Token [type=" + type + ", text=" + text + "]";
+        return "Token [type= " + type + ", text: " + text + " ]";
     }
 }
